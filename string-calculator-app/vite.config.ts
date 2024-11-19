dbf2ba3a20a6fce882fb6cf702811c7cd119a36f
Afajal Meman <afzal@smilebots.com>
2024-11-20 01:15:06 +0530

    vite test setup

--- string-calculator-app/vite.config.ts
+++ string-calculator-app/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  test: {
+    globals: true,  // Use globals for testing
+    environment: 'jsdom',  // Environment for testing
+    setupFiles: './test/setup.ts',  // Setup file
+    include: ['test/**/*.test.tsx'],  // Pattern to match test files
+  },
+  plugins: [react()]
+});
