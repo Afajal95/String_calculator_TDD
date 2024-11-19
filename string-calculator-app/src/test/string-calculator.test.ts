9376ac5a07750f52769589bcad8996a742b0aea6
Afajal Meman <afzal@smilebots.com>
2024-11-20 01:51:26 +0530

    third test case with multiple numbers passed to function

--- string-calculator-app/src/test/string-calculator.test.ts
+++ string-calculator-app/src/test/string-calculator.test.ts
@@ -11,4 +11,7 @@ describe("StringCalculator", () => {
   it("should return the sum of two numbers when two numbers are passed", () => {
     expect(stringCalculator("1,2")).toBe(3);
   });
+  it("should return the sum of multiple numbers when multiple numbers are passed", () => {
+    expect(stringCalculator("1,2,3")).toBe(6);
+  });
 });
