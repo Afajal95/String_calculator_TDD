f301a813a4d329a16b1d0443ab93a8bcc0c899ce
Afajal Meman <afzal@smilebots.com>
2024-11-20 01:36:32 +0530

    initial test file for string cal util

--- string-calculator-app/src/test/string-calculator.test.ts
+++ string-calculator-app/src/test/string-calculator.test.ts
@@ -1,9 +1,8 @@
-import { StringCalculator } from "../utils/string-calculator"
+import { stringCalculator } from "../utils/string-calculator"
 import { describe, it, expect } from 'vitest';
 
 describe("StringCalculator", () => {
   it("should return 0 when empty string is passed", () => {
-    const stringCalculator = new StringCalculator();
-    expect(stringCalculator.calculate("")).toBe(0);
+    expect(stringCalculator("")).toBe(0);
   });
 });
